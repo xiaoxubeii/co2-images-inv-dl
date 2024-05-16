289d55a42ef744674ee96338decbd66a11d03419
Bei Xu <xubei@rongqiglobal.com>
2024-05-16 20:03:44 +0100

    first commit

---
+++ include/callbacks.py
@@ -0,0 +1,74 @@
+# ----------------------------------------------------------------------------
+# author: joffrey.dumont@enpc.fr or joffreydumont@hotmail.fr
+# created: 2021-2023
+# laboratory: CEREA,  École des Ponts and EDF R&D, Île-de-France, France
+# project: Prototype system for a Copernicus C02 monitoring service (CoCO2)
+# ----------------------------------------------------------------------------
+
+import os
+import sys
+
+import numpy as np
+import tensorflow as tf
+from omegaconf import DictConfig, OmegaConf
+from tensorflow import keras
+from wandb.integration.keras import WandbCallback
+
+import wandb
+
+
+
+def get_modelcheckpoint(get: bool, cbs: list, filepath="w_best.h5") -> list:
+    """Add modelcheckpoint to callbacks list if get."""
+    if get:
+        modelcheckpoint_cb = tf.keras.callbacks.ModelCheckpoint(
+            filepath=filepath,
+            save_weights_only=False,
+            monitor="val_loss",
+            mode="auto",
+            save_best_only=True,
+            verbose=1,
+        )
+        cbs.append(modelcheckpoint_cb)
+    else:
+        pass
+    return cbs
+
+
+def get_lrscheduler(get: bool, cbs: list) -> list:
+    """Add reducelronplateau to callbacks list if get."""
+    if get:
+        reducelronplateau_cb = tf.keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss",
+            factor=0.5,
+            patience=20,
+            verbose=0,
+            min_delta=5e-3,
+            cooldown=0,
+            min_lr=5e-5,
+        )
+        cbs.append(reducelronplateau_cb)
+    else:
+        pass
+    return cbs
+
+
+def get_earlystopping(get: bool, cbs: list) -> list:
+    """Add earlystopping to callbacks list if get."""
+    if get:
+        earlystopping_cb = tf.keras.callbacks.EarlyStopping(
+            monitor="val_loss",
+            min_delta=5e-4,
+            patience=50,
+            verbose=0,
+            mode="auto",
+            baseline=None,
+            restore_best_weights=True,
+        )
+        cbs.append(earlystopping_cb)
+    else:
+        pass
+    return cbs
+
+
+
---
+++ test.py
@@ -0,0 +1,44 @@
+import models.reg as rm
+import model_eval
+import matplotlib_functions as mympf
+import sys
+
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras as keras
+import xarray as xr
+from cmcrameri import cm
+from hydra import compose, initialize
+from omegaconf import OmegaConf, DictConfig
+from scipy.optimize import differential_evolution
+from sklearn import preprocessing
+import os
+
+sys.path.append(
+    "/Users/xiaoxubeii/Program/go/src/github.com/co2-images-inv-dl")
+# from Data import Data_eval
+
+mympf.setMatplotlibParam()
+plt.viridis()
+dir_res = "/Users/xiaoxubeii/Program/go/src/github.com/co2-images-inv-dl/res/inversion/inv_lip"
+name_model = dir_res
+path_eval_nc = "/Users/xiaoxubeii/Downloads/data_paper_inv_pp/all_hotspots_but_lip/train_dataset.nc"
+data = model_eval.get_data_for_inversion(
+    dir_res,
+    path_eval_nc,
+)
+
+model = model_eval.get_inversion_model(
+    os.path.join(dir_res, name_model), name_w="w_last.keras"
+)
+metrics = model_eval.get_inv_metrics_model_on_data(model, data)
+print("mae:", np.mean(metrics["mae"]))
+print("mape:", np.mean(metrics["mape"]))
+
+model_eval.plot_inversion_examples(data, metrics["mae"], metrics["mape"], model)
+model_eval.plot_inversion_examples(data, metrics["mae"], metrics["mape"], model)
+
+plt.show()  
