9dc9ec3e3115774e6ad08f0db526a43866f97caa
Bei Xu <xubei@rongqiglobal.com>
2024-07-26 15:17:40 +0100

    u

--- load_tpu.py
+++ load_tpu.py
@@ -1,4 +1,24 @@
+import math, re, os
 import tensorflow as tf
-tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
-tf.config.experimental_connect_to_cluster(tpu)
-tf.tpu.experimental.initialize_tpu_system(tpu)
+import numpy as np
+from matplotlib import pyplot as plt
+from kaggle_datasets import KaggleDatasets
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+print("Tensorflow version " + tf.__version__)
+AUTO = tf.data.experimental.AUTOTUNE
+
+# Detect TPU, return appropriate distribution strategy
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver() 
+    print('Running on TPU ', tpu.master())
+except ValueError:
+    tpu = None
+
+if tpu:
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.experimental.TPUStrategy(tpu)
+else:
+    strategy = tf.distribute.get_strategy() 
+
+print("REPLICAS: ", strategy.num_replicas_in_sync)
