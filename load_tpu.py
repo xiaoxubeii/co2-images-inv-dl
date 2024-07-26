9614e884106405c31f22ebfa0e6134942592b869
Bei Xu <xubei@rongqiglobal.com>
2024-07-26 15:23:25 +0100

    u

--- load_tpu.py
+++ load_tpu.py
@@ -1,24 +1,29 @@
-import math, re, os
 import tensorflow as tf
-import numpy as np
-from matplotlib import pyplot as plt
-from kaggle_datasets import KaggleDatasets
-from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+import hydra
+from omegaconf import DictConfig, OmegaConf
 print("Tensorflow version " + tf.__version__)
 AUTO = tf.data.experimental.AUTOTUNE
 
-# Detect TPU, return appropriate distribution strategy
-try:
-    tpu = tf.distribute.cluster_resolver.TPUClusterResolver() 
-    print('Running on TPU ', tpu.master())
-except ValueError:
-    tpu = None
 
-if tpu:
-    tf.config.experimental_connect_to_cluster(tpu)
-    tf.tpu.experimental.initialize_tpu_system(tpu)
-    strategy = tf.distribute.experimental.TPUStrategy(tpu)
-else:
-    strategy = tf.distribute.get_strategy() 
+@hydra.main(config_path="cfg", config_name="config")
+def main_train(cfg: DictConfig):
 
-print("REPLICAS: ", strategy.num_replicas_in_sync)
+    # Detect TPU, return appropriate distribution strategy
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+        print('Running on TPU ', tpu.master())
+    except ValueError:
+        tpu = None
+
+    if tpu:
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.experimental.TPUStrategy(tpu)
+    else:
+        strategy = tf.distribute.get_strategy()
+
+    print("REPLICAS: ", strategy.num_replicas_in_sync)
+
+
+if __name__ == "__main__":
+    main_train()
