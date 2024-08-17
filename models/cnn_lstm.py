9323fd59efe5159289d5fdb14a73ad55b14fef7d
Bei Xu <xubei@rongqiglobal.com>
2024-08-17 18:10:31 +0100

    u

--- models/cnn_lstm.py
+++ models/cnn_lstm.py
@@ -11,15 +11,17 @@ from tensorflow.keras.layers import LSTM
 
 def cnn_lstm(input_shape,  drop_CNN=0, drop_dense=0.2, kernel_size=(3, 3)):
     model = Sequential()
-    model.add(TimeDistributed(Conv2D(64, kernel_size=kernel_size,
+    model.add(TimeDistributed(Conv2D(128, kernel_size=kernel_size,
               padding="same", activation="relu"), input_shape=input_shape))
     model.add(Dropout(drop_CNN))
     model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2), padding='same')))
     model.add(TimeDistributed(BatchNormalization()))
 
-    model.add(TimeDistributed(Flatten()))
-    model.add(LSTM(units=64, return_sequences=True))
-    model.add(LSTM(units=64, return_sequences=False))
+    # model.add(TimeDistributed(Flatten()))
+    model.add(LSTM(units=128, return_sequences=True))
+    model.add(LSTM(units=128, return_sequences=False))
+    model.add(Flatten())
+
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(drop_dense))
     # model.add(Dense(1, activation='linear'))
