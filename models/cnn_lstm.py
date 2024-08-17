190cc0374e6973d29253a5c6b1e1df102d251f75
Bei Xu <xubei@rongqiglobal.com>
2024-08-17 18:15:07 +0100

    u

--- models/cnn_lstm.py
+++ models/cnn_lstm.py
@@ -17,12 +17,10 @@ def cnn_lstm(input_shape,  drop_CNN=0, drop_dense=0.2, kernel_size=(3, 3)):
     model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2), padding='same')))
     model.add(TimeDistributed(BatchNormalization()))
 
-    # model.add(TimeDistributed(Flatten()))
+    model.add(TimeDistributed(Flatten()))
     model.add(LSTM(units=128, return_sequences=True))
     model.add(LSTM(units=128, return_sequences=False))
-    model.add(Flatten())
-
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(drop_dense))
+    # model.add(Dense(128, activation='relu'))
+    # model.add(Dropout(drop_dense))
     # model.add(Dense(1, activation='linear'))
     return model
